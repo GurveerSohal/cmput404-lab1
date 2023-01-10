1b2793dc998d0dfcd6bb7b1c11d6329ec5088d70
Gurveer Sohal <gurveersohal433@gmail.com>
2023-01-09 17:52:33 -0700

    modified

--- printversion.py
+++ printversion.py
@@ -1,5 +1,5 @@
 import os
 import requests
 # os.system("pip show requests")
-res = requests.get("http://www.google.com/")
-print(res)
+res = requests.get("https://raw.githubusercontent.com/GurveerSohal/cmput404-lab1/main/printversion.py")
+print(res.content.decode())
